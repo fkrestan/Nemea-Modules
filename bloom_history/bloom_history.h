607c6c42d84f65022bf40d9e1db59eea6853e3ad
Filip Krestan <krestfi1@fit.cvut.cz>
2019-01-11 13:42:24 +0100

    Disable debug output by default for prefix_tags, bloom_history

--- bloom_history/bloom_history.h
+++ bloom_history/bloom_history.h
@@ -16,7 +16,9 @@ UR_FIELDS (
    uint32 PREFIX_TAG
 )
 
-#define DEBUG 1
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 #define debug_print(fmt, ...) \
         do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
--- prefix_tags/prefix_tags.h
+++ prefix_tags/prefix_tags.h
@@ -4,7 +4,9 @@
 static const int INTERFACE_IN = 0;
 static const int INTERFACE_OUT = 0;
 
-#define DEBUG 1
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 #define debug_print(fmt, ...) \
         do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
